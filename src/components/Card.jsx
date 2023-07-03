dd9b6ba3481b40726bdf6ee7a09b0e698b18a7f8
Koray KOKSAL <tkkoksal@icloud.com>
2023-07-03 23:08:16 +0300

    card div scss

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,15 +6,27 @@ const Card = ({data}) => {
 
   console.log(data);
 
+  // container alanını burada oluşturur
   return <div className={CardStyle.container}>
 
 
     {data.map((item)=>{
 
+      const {id,name,job,img,comment}= item
 
-      return (<div className={CardStyle.card}>
+      // card divi burada oluşturur
+      return (<div className={CardStyle.card} key={id}>
 
+        <h1>{name}</h1>
+        <h3>{job}</h3>
+        <p>{comment}</p>
+        <img src={img} alt="" />
 
+        <div className={CardStyle.buttons}>
+          <button className={CardStyle.small}>Small</button>
+          <button className={CardStyle.large}>Large</button>
+        </div>
+        
       </div>)
 
     })}
