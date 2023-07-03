01f1ddb7d255344c36869be18bed3de4f5a77198
Koray KOKSAL <tkkoksal@icloud.com>
2023-07-03 23:13:02 +0300

    made by kk

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import FooterStyle from "../scss/footer.module.scss"
 const Footer = () => {
   return (
     <footer className={FooterStyle["footer"]}>
-      <p>Copyright by Clarusway {new Date().getFullYear()}</p>
+      <p>Made by KK 👑 {new Date().getFullYear()}</p>
     </footer>
   )
 }
